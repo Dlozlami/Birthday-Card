889cd782d189d564e5f7e8ff0c3872e06bca94c9
UX DLOZI <40693444+Dlozlami@users.noreply.github.com>
2023-07-11 23:00:47 +0300

    final

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Slider from '../components/slider';
+import Head from "expo-router/head";
 
 export default function App(){
-  return (
+  return (<>
+
     <SafeAreaView>
       <Slider />
     </SafeAreaView>
-  );
+  </>);
 };
 
 const styles = StyleSheet.create({});
