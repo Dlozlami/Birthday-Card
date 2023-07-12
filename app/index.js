1b18e951c2632154d427ce107fec5972739f962b
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-07-12 10:32:30 +0200

    head tag

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import Slider from '../components/slider';
-import Head from "expo-router/head";
+
 
 export default function App(){
   return (<>
